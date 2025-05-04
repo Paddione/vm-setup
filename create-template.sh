2423f00b971e07a8143b71839ec9a1770d10e5bd
Paddione <82854371+Paddione@users.noreply.github.com>
2025-05-04 02:11:28 +0200

    Create create-template.sh

---
+++ create-template.sh
@@ -0,0 +1,7 @@
+qm create 9000 --memory 8196 --cores 4 --name ubuntu-cloud --net0 virtio,bridge=vmbr0
+qm importdisk 9000 jammy-server-cloudimg-amd64.img local-lvm
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
+qm set 9000 --ide2 local-lvm:cloudinit
+qm set 9000 --boot c --bootdisk scsi0
+qm set 9000 --serial0 socket --vga serial0
+qm template 9000
